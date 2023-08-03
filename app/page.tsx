d3a01d960abafdc0ac68aa02bc0a683fe1d87d6f
Zaid-maker <pzhafeez@gmail.com>
2023-08-03 12:53:07 +0500

    ✨ feat(page.tsx): add a button component to the Home page for improved user interaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">Genius AI</div>;
+  return (
+    <div className="">
+      <Button>Click me</Button>
+    </div>
+  );
 }
