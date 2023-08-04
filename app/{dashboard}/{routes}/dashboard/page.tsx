8fa47955d53fc8a1b8e791f058667cadc9cd060b
Zaid-maker <pzhafeez@gmail.com>
2023-08-04 17:05:05 +0500

    ✨ feat(dashboard/page.tsx): add DashboardPage component to display a button in the dashboard page

--- app/{dashboard}/{routes}/dashboard/page.tsx
+++ app/{dashboard}/{routes}/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <div className="">
       <Button>Click me</Button>
