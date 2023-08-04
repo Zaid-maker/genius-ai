111db1d446f3c4ce10d8fb2f572d49f2707600fd
Zaid-maker <pzhafeez@gmail.com>
2023-08-04 23:14:22 +0500

    🔧 refactor(layout.tsx): update DashboardLayout component to include a sidebar for larger screens

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900"></div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
