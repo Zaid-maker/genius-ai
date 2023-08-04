820429f1da96ca67a2a1c7ebdcd230abea9bf902
Zaid-maker <pzhafeez@gmail.com>
2023-08-04 17:13:04 +0500

    🔥 refactor(dashboard/page.tsx): remove unused imports and simplify DashboardPage component

--- app/{dashboard}/{routes}/dashboard/page.tsx
+++ app/{dashboard}/{routes}/dashboard/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function DashboardPage() {
-  return (
-    <div className="">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <p>Dashboard Page</p>;
 }
