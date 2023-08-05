3d0c7da2b568641a720a17baa7e7af0253a6d9eb
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 21:53:16 +0500

    ✨ feat(Mobile-Sidebar.tsx): add MobileSidebar component to handle mobile sidebar functionality 🔥 refactor(Navbar.tsx): remove MobileSidebar button from Navbar component to improve code organization and separation of concerns

---
+++ components/Mobile-Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+
+const MobileSidebar = () => {
+  return (
+    <Button variant="ghost" size="icon" className="md:hidden">
+      <Menu />
+    </Button>
+  );
+};
+
+export default MobileSidebar;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
