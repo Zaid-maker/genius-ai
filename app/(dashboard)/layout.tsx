42645c558e1a320efae6bd89478edb466c06cddd
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 22:22:12 +0500

    🔀 refactor(layout.tsx): import Sidebar component from specific file path to improve clarity and maintainability

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
+import { Sidebar } from "@/components/Sidebar";
 import React from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
