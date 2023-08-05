537d82baab715d44fe7b75ac2e0fd3359ee50e06
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 15:18:48 +0500

    🎨 style(layout.tsx): add sidebar to the dashboard layout for better navigation experience

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900"></div>
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        Hello Sidebar
+      </div>
+      <main className="md:pl-72">{children}</main>
     </div>
   );
 };
