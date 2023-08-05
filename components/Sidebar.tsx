daeda736b695bdd02c73f717f466f07f69ce5b56
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 20:07:24 +0500

    🎨 style(Sidebar.tsx): add a container div for the logo image to improve layout and spacing 🐛 fix(Sidebar.tsx): wrap the Image component in a div with a fixed height and width to prevent layout issues

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,7 +9,9 @@ const Sidebar = () => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <Image src="/logo.png" alt="logo" fill />
+          <div className="relative h-8 w-8 mr-4">
+            <Image src="/logo.png" alt="logo" fill />
+          </div>
         </Link>
         <h1 className="text-2xl font-bold">Genius AI</h1>
       </div>
