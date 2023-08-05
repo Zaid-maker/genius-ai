247cc42648b5c5c5dad4cbca04142aa0790f8f4a
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 19:57:59 +0500

    🎨 style(Sidebar.tsx): format code for better readability and consistency 🔧 chore(Sidebar.tsx): import necessary dependencies for Sidebar component ✨ feat(Sidebar.tsx): add logo and title to Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <Image src="/logo.png" alt="logo" fill />
+        </Link>
+        <h1 className="text-2xl font-bold">Genius AI</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
