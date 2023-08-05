6d5bc0a8d9f621f3fef81aae1d3522c4e4e8c13e
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 22:16:50 +0500

    🎨 style(Navbar.tsx): add MobileSidebar component to the Navbar for improved mobile navigation experience

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
+import { MobileSidebar } from "./Mobile-Sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
