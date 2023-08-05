3ac939225f25427b1d7f0ba4edeb6afb2c9acafa
Zaid-maker <pzhafeez@gmail.com>
2023-08-05 21:34:52 +0500

    🎨 style(Sidebar.tsx): add spacing between sidebar links for better readability and visual separation 🔧 chore(Sidebar.tsx): refactor sidebar links to use map function for cleaner code and easier maintenance

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -42,6 +42,25 @@ const Sidebar = () => {
             Genius AI
           </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
