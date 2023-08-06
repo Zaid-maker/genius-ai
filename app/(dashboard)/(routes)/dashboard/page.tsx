56c9b041f7901778fec84aae4ccca5bd59344901
Zaid-maker <pzhafeez@gmail.com>
2023-08-06 23:50:19 +0500

    🎨 style(page.tsx): improve the layout of the DashboardPage component by adding a heading and some spacing

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
