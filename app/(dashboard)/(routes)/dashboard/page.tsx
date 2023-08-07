03619e63a4a0dbacb05e658e5e5354d51bcfe4b8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-07 16:42:14 +0000

    Add card styling for tools

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { MessageSquare } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, MessageSquare } from "lucide-react";
 import React from "react";
 
 const tools = [
@@ -26,7 +28,20 @@ const DashboardPage = () => {
       </div>
 
       <div className="px-4 md:px:20 lg:px-32 space-y-4">
-        {tools.map((tool) => {})}
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className=" w-5 h-5" />
+          </Card>
+        ))}
       </div>
     </div>
   );
