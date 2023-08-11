2bc3dc9864d1d094361ec501aa4909187b14e69c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-11 16:44:04 +0000

    add icon, h2, description in Heading

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import React from "react";
 
@@ -12,9 +13,21 @@ interface HeadingProps {
 export const Heading = ({
   title,
   description,
-  icon,
+  icon: Icon,
   iconColor,
   bgColor,
 }: HeadingProps) => {
-  return <div>{title}</div>;
+  return (
+    <>
+      <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+          <Icon className={cn("w-10 h-10", iconColor)} />
+        </div>
+        <div>
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <p className="text-sm text-muted-foreground">{description}</p>
+        </div>
+      </div>
+    </>
+  );
 };
