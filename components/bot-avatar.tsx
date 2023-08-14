4d9bee4584279b7435d885edb4ee2a24a8649aad
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-14 09:11:11 +0000

    🔧 chore(bot-avatar.tsx): fix typo in class attribute names to match CSS class names 🎨 style(bot-avatar.tsx): update class attribute names to follow consistent naming convention with other components

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -2,8 +2,8 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const BotAvatar = () => {
   return (
-    <Avatar classname="h-8 w-8">
-      <AvatarImage classname="p-1" src="/logo.png" />
+    <Avatar className="h-8 w-8">
+      <AvatarImage className="p-1" src="/logo.png" />
     </Avatar>
   );
 };
