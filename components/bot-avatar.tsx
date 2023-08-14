9d3f3047c6406ffa9a7f87e7dddc29612d15461f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-14 09:01:13 +0000

    ✨ feat(bot-avatar.tsx): add BotAvatar component to display a bot avatar with a logo image

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+export const BotAvatar = () => {
+  return (
+    <Avatar classname="h-8 w-8">
+      <AvatarImage classname="p-1" src="/logo.png" />
+    </Avatar>
+  );
+};
