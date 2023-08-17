12d7308a63be41b3e478b164c2f6cf45ce37deb8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-17 08:55:39 +0000

    🔀 chore(constants.ts): add amountOptions array to provide options for selecting the number of photos in the form schema

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -7,3 +7,26 @@ export const formSchema = z.object({
     amount: z.string().min(1),
     resolution: z.string().min(1)
 })
+
+export const amountOptions = [
+    {
+        value: '1',
+        label: "1 Photo"
+    },
+    {
+        value: '2',
+        label: "2 Photos"
+    },
+    {
+        value: '3',
+        label: "3 Photos"
+    },
+    {
+        value: '4',
+        label: "4 Photos"
+    },
+    {
+        value: '5',
+        label: "5 Photos"
+    }
+]
